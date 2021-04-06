e714197d8590a69dea82a13672bd328efb7fc008
sviatoslavveklenko <sviatoslavveklenko@gmail.com>
2021-04-06 17:18:22 +0300

    Сверстал секцию _price ,также сделал анимации я для других секций страницы

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
-	var button = $('#button')
-	var modal = $('#modal')
-	var close = $('#close')
-	button.on('click',function(){
-		modal.addClass('modal_active')
-	})
-	close.on('click',function(){
-		modal.removeClass('modal_active')
-	})
+// 	var button = $('#button')
+// 	var modal = $('#modal')
+// 	var close = $('#close')
+// 	button.on('click',function(){
+// 		modal.addClass('modal_active')
+// 	})
+// 	close.on('click',function(){
+// 		modal.removeClass('modal_active')
+// 	})
+	
 	var up = $('#up')
 	 if ($(window).scrollTop() > 300) {
        up.addClass('show');
@@ -16,7 +17,8 @@ $(document).ready(function(){
      } 
 	up.on('click',function(){
     $('html, body').animate({scrollTop:0}, '300');
-  }); 
+  });
+  
 
 		})
 
